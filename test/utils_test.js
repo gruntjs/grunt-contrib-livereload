edaefc18274d025837793adc3a6dd9bb5f27a96c
Brian Lai <brian.lai.ym@gmail.com>
2013-03-07 20:29:24 -0800

    re-write Response.end() instead of Response.write()

--- test/utils_test.js
+++ test/utils_test.js
@@ -12,10 +12,8 @@ describe('livereloadSnippet', function () {
     utils.livereloadSnippet(req, res, next);
   });
 
-  it('should re-write response write function', function (done) {
+  it('should re-write response end function', function (done) {
     var req = { url: '/' };
-    var headers = {};
-    var implicitHeaderCalled = false;
     var writeString = '';
     var res = {
       socket: {
@@ -27,60 +25,17 @@ describe('livereloadSnippet', function () {
           }
         }
       },
-      write: function (string) {
+      end: function (string) {
         writeString = string;
-      },
-      setHeader: function (header, value) {
-        headers[header] = value;
-      },
-      _implicitHeader: function () {
-        implicitHeaderCalled = true;
       }
     };
     var next = function () {
       done();
     };
     utils.livereloadSnippet(req, res, next);
-    res.write('<body>我能吞下玻璃而不伤身体。</body>','ascii');
+    res.end('<body>我能吞下玻璃而不伤身体。</body>','ascii');
     // original write is called
     assert.ok(writeString.match(/livereload snippet/));
-    assert.equal(headers['content-length'], 206);
-    assert.ok(implicitHeaderCalled);
-  });
-
-  it('should send correct content-length for html containing multi byte', function(done) {
-    var req = { url: '/' };
-    var headers = {};
-    var implicitHeaderCalled = false;
-    var writeString = '';
-    var res = {
-      socket: {
-        server: {
-          address: function () {
-            return {
-              port: 12345
-            };
-          }
-        }
-      },
-      write: function (string) {
-        writeString = string;
-      },
-      setHeader: function (header, value) {
-        headers[header] = value;
-      },
-      _implicitHeader: function () {
-        implicitHeaderCalled = true;
-      }
-    };
-    var next = function () {
-      done();
-    };
-    utils.livereloadSnippet(req, res, next);
-    res.write('<h1>Foo</h1><p>Multibyte characters here: ääää ööööö åååååå</p><p>Some html here</p>');
-    // original write is called
-    assert.equal(headers['content-length'], 99);
-    assert.ok(implicitHeaderCalled);
   });
 
   it('should do nothing if requested page is not an HTML page', function (done) {
