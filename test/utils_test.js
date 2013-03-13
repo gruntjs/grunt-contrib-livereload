f41d53646a51ebc6a2d62aa8c43d239f90a6684c
Brian Lai <brian.lai.ym@gmail.com>
2013-03-12 22:14:11 -0700

    overriding res.send (in addition to res.write). It looks like res.send and res.write covers most scenarios for connect/express to send a body of html in some way

--- test/utils_test.js
+++ test/utils_test.js
@@ -12,8 +12,10 @@ describe('livereloadSnippet', function () {
     utils.livereloadSnippet(req, res, next);
   });
 
-  it('should re-write response end function', function (done) {
+  it('should re-write response write function', function (done) {
     var req = { url: '/' };
+    var headers = {};
+    var implicitHeaderCalled = false;
     var writeString = '';
     var res = {
       socket: {
@@ -25,17 +27,60 @@ describe('livereloadSnippet', function () {
           }
         }
       },
-      end: function (string) {
+      write: function (string) {
         writeString = string;
+      },
+      setHeader: function (header, value) {
+        headers[header] = value;
+      },
+      _implicitHeader: function () {
+        implicitHeaderCalled = true;
       }
     };
     var next = function () {
       done();
     };
     utils.livereloadSnippet(req, res, next);
-    res.end('<body>我能吞下玻璃而不伤身体。</body>','ascii');
+    res.write('<body>我能吞下玻璃而不伤身体。</body>','ascii');
     // original write is called
     assert.ok(writeString.match(/livereload snippet/));
+    assert.equal(headers['content-length'], 206);
+    assert.ok(implicitHeaderCalled);
+  });
+
+  it('should send correct content-length for html containing multi byte', function(done) {
+    var req = { url: '/' };
+    var headers = {};
+    var implicitHeaderCalled = false;
+    var writeString = '';
+    var res = {
+      socket: {
+        server: {
+          address: function () {
+            return {
+              port: 12345
+            };
+          }
+        }
+      },
+      write: function (string) {
+        writeString = string;
+      },
+      setHeader: function (header, value) {
+        headers[header] = value;
+      },
+      _implicitHeader: function () {
+        implicitHeaderCalled = true;
+      }
+    };
+    var next = function () {
+      done();
+    };
+    utils.livereloadSnippet(req, res, next);
+    res.write('<h1>Foo</h1><p>Multibyte characters here: ääää ööööö åååååå</p><p>Some html here</p>');
+    // original write is called
+    assert.equal(headers['content-length'], 99);
+    assert.ok(implicitHeaderCalled);
   });
 
   it('should do nothing if requested page is not an HTML page', function (done) {
@@ -52,5 +97,64 @@ describe('livereloadSnippet', function () {
     utils.livereloadSnippet(req, res, next);
     res.write('fred');
     assert.equal('fred', writeString);
+  });
+
+  it('should re-write response send function', function (done) {
+    var req = { url: '/' };
+    var sendBody = '';
+    var res = {
+      send: function (body) {
+        sendBody = body;
+      }
+    };
+    var next = function () {
+      done();
+    };
+    utils.livereloadSnippet(req, res, next);
+    res.send('<body>我能吞下玻璃而不伤身体。</body>');
+    // original send is called
+    assert.ok(sendBody.match(/livereload snippet/));
+  });
+
+  it('should do nothing if sending just the http status code', function (done) {
+    var req = { url: '/' };
+    var statusCode = -1;
+    var res = {
+      send: function (body) {
+        statusCode = body;
+      }
+    };
+    var next = function () {
+      done();
+    };
+    utils.livereloadSnippet(req, res, next);
+    res.send(200);
+    assert.equal(200, statusCode);
+  });
+
+  it('should support optional status code as first or second argument when doing res.send()', function (done) {
+    var req = { url: '/' };
+    var statusCode = -1;
+    var sendBody = '';
+    var res = {
+      send: function (body) {
+        statusCode = body;
+        sendBody = arguments[1];
+      }
+    };
+    var next = function() {
+      done();
+    };
+    utils.livereloadSnippet(req, res, next);
+    res.send(200, '<body>hello</body>');
+    assert.ok(sendBody.match(/livereload snippet/));
+    assert.equal(200, statusCode);
+
+    // resetting for case 2
+    statusCode = -1;
+    sendBody = '';
+    res.send('<body>hello</body>', 200);
+    assert.ok(sendBody.match(/livereload snippet/));
+    assert.equal(200, statusCode);
   });
 });
