dd405184557cbd8ffdcaa400c50c13436f7d6d27
Ian Anderson <ian@ianize.com>
2013-03-26 14:20:54 -0500

    Fixes #25 #26: Snippet truncation issues
    Squashed commit of the following:
    
    commit c0d49f6f72efbca1b70be3962e221f049f0f7b35
    Author: Ian Anderson <ian@ianize.com>
    Date:   Tue Mar 26 13:09:38 2013 -0500
    
        Fixes truncation on piped writes
    
    commit fe775b14efffd835ced7b305c1595aa4b28be018
    Author: Ian Anderson <ian@ianize.com>
    Date:   Tue Mar 19 00:08:16 2013 -0500
    
        Fixes #26: Support Express response send function
    
    commit 55083563e2d5543db95eeaca1d37ddd27cbd953a
    Author: Ian Anderson <ian@ianize.com>
    Date:   Tue Mar 12 18:22:53 2013 -0500
    
        Fixes #25: 40kb limit on html files

--- test/utils_test.js
+++ test/utils_test.js
@@ -27,8 +27,12 @@ describe('livereloadSnippet', function () {
           }
         }
       },
+      end: function (string) {
+        implicitHeaderCalled = true;
+        writeString += string;
+      },
       write: function (string) {
-        writeString = string;
+        writeString += string;
       },
       setHeader: function (header, value) {
         headers[header] = value;
@@ -41,12 +45,56 @@ describe('livereloadSnippet', function () {
       done();
     };
     utils.livereloadSnippet(req, res, next);
-    res.write('<body>我能吞下玻璃而不伤身体。</body>','ascii');
+    res.write('<body>我能吞下玻璃而不伤身体。</body>');
+    res.end();
+
     // original write is called
     assert.ok(writeString.match(/livereload snippet/));
-    assert.equal(headers['content-length'], 206);
+    assert.equal(headers['content-length'], 230);
     assert.ok(implicitHeaderCalled);
   });
+
+  it('should support Express response send function', function (done) {
+    var req = { url: '/' };
+    var headers = {};
+    var implicitHeaderCalled = false;
+    var writeString = '';
+    var res = {
+      socket: {
+        server: {
+          address: function () {
+            return {
+              port: 12345
+            };
+          }
+        }
+      },
+      end: function (string) {
+        implicitHeaderCalled = true;
+        writeString += string;
+      },
+      setHeader: function (header, value) {
+        headers[header] = value;
+      },
+      _implicitHeader: function () {
+        implicitHeaderCalled = true;
+      }
+    };
+    res.send = function (body) {
+      res.end(body);
+    };
+    var next = function () {
+      done();
+    };
+    utils.livereloadSnippet(req, res, next);
+    res.send('<body>我能吞下玻璃而不伤身体。</body>');
+
+    // original write is called
+    assert.ok(writeString.match(/livereload snippet/));
+    assert.equal(headers['content-length'], 230);
+    assert.ok(implicitHeaderCalled);
+  });
+
 
   it('should send correct content-length for html containing multi byte', function(done) {
     var req = { url: '/' };
@@ -63,6 +111,9 @@ describe('livereloadSnippet', function () {
           }
         }
       },
+      end: function () {
+        implicitHeaderCalled = true;
+      },
       write: function (string) {
         writeString = string;
       },
@@ -78,6 +129,102 @@ describe('livereloadSnippet', function () {
     };
     utils.livereloadSnippet(req, res, next);
     res.write('<h1>Foo</h1><p>Multibyte characters here: ääää ööööö åååååå</p><p>Some html here</p>');
+    res.end();
+    // original write is called
+    assert.equal(headers['content-length'], 99);
+    assert.ok(implicitHeaderCalled);
+  });
+
+  it('should send correct length after piped writes', function (done) {
+    var req = { url: '/' };
+    var headers = {};
+    var header = false;
+    var implicitHeaderCalled = false;
+    var writeString = '';
+    var res = {
+      socket: {
+        server: {
+          address: function () {
+            return {
+              port: 12345
+            };
+          }
+        }
+      },
+      on: function() {},
+      once: function() {},
+      removeListener: function() {},
+      emit: function() {},
+      writable: true,
+      write: function (string) {
+        writeString = string;
+      },
+      writeHeader: function () {
+        header = true;
+      },
+      setHeader: function (header, value) {
+        assert.ok(header);
+        headers[header] = value;
+      }
+    };
+
+    var next = function () {};
+    var stream = require('fs').createReadStream('test/fixtures/index.html');
+
+    res.end = function () {
+      if (!implicitHeaderCalled) {
+        assert.equal(headers['content-length'], 73557);
+        done();
+      }
+      implicitHeaderCalled = true;
+    };
+
+    utils.livereloadSnippet(req, res, next);
+    stream.pipe(res);
+  });
+
+  it('should send correct content-length after multiple writes', function(done) {
+    var req = { url: '/' };
+    var headers = {};
+    var header = false;
+    var implicitHeaderCalled = false;
+    var writeString = '';
+    var res = {
+      socket: {
+        server: {
+          address: function () {
+            return {
+              port: 12345
+            };
+          }
+        }
+      },
+      end: function () {
+        implicitHeaderCalled = true;
+      },
+      write: function (string) {
+        writeString = string;
+      },
+      writeHeader: function () {
+        header = true;
+      },
+      setHeader: function (header, value) {
+        assert.ok(header);
+        headers[header] = value;
+      },
+      _implicitHeader: function () {
+        implicitHeaderCalled = true;
+      }
+    };
+    var next = function () {
+      done();
+    };
+
+    utils.livereloadSnippet(req, res, next);
+    res.write('<h1>Foo</h1>');
+    res.write('<p>Multibyte characters here: ääää ööööö åååååå</p>');
+    res.write('<p>Some html here</p>');
+    res.end();
     // original write is called
     assert.equal(headers['content-length'], 99);
     assert.ok(implicitHeaderCalled);
